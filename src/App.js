f95df4346c5cc9b2b9c16978a64504ad924196ea
moritz_tomasi <tomasi.moritz@gmail.com>
2021-08-16 13:09:54 +0200

    setup tailwind and implement theme toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch } from "@headlessui/react";
+import { useEffect, useState } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    function prefersDarkTheme() {
+      return (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      );
+    }
+
+    setEnabled(prefersDarkTheme);
+  }, []);
+
+  useEffect(() => {
+    if (enabled) {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+
+      localStorage.theme = "light";
+    }
+  }, [enabled]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className="relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out bg-gray-200 border-2 border-transparent rounded-full cursor-pointer w-11 ring-0 dark:bg-gray-600"
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        className={classNames(
+          enabled ? "translate-x-5" : "translate-x-0",
+          "pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+        )}
+      >
+        <span
+          className={classNames(
+            enabled
+              ? "opacity-0 ease-out duration-100"
+              : "opacity-100 ease-in duration-200",
+            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+          )}
+          aria-hidden="true"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <svg
+            className="w-3 h-3"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <line x1="12" y1="1" x2="12" y2="3" />
+            <line x1="12" y1="21" x2="12" y2="23" />
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+            <line x1="1" y1="12" x2="3" y2="12" />
+            <line x1="21" y1="12" x2="23" y2="12" />
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+          </svg>
+        </span>
+        <span
+          className={classNames(
+            enabled
+              ? "opacity-100 ease-in duration-200"
+              : "opacity-0 ease-out duration-100",
+            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+          )}
+          aria-hidden="true"
+        >
+          <svg
+            className="w-3 h-3 text-gray-400 bg-white"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+          </svg>
+        </span>
+      </span>
+    </Switch>
   );
 }
 
